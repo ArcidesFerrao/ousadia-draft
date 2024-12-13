9b910e790903ca60df590e6c9cec53f353c64813
ArcidesFerrao <cidesferrao@gmail.com>
2024-12-13 13:35:30 +0200

    icon path

--- src/components/NavIcon.tsx
+++ src/components/NavIcon.tsx
@@ -1,10 +1,12 @@
-import logo from "./src/app/assets/icon.png";
+// import logo from "./src/app/assets/icon.png";
 import Image from "next/image";
 
+const iconPath = "/assets/icon.png";
+
 export default function NavIcon() {
-  return <Image src={logo} width={120} height={120} alt="Ousadia" />;
+  return <Image src={iconPath} width={120} height={120} alt="Ousadia" />;
 }
 
 export function FooterIcon() {
-  return <Image src={logo} width={200} height={200} alt="Ousadia" />;
+  return <Image src={iconPath} width={200} height={200} alt="Ousadia" />;
 }
