6358b6bc666a068b7934c8a3af9890484b498ea7
ArcidesFerrao <cidesferrao@gmail.com>
2024-12-13 14:44:35 +0200

    buy button switch

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,20 +1,36 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
 
 export default function ProductCard({ ...props }) {
+  const [hovered, setHovered] = useState(false);
   return (
     <div className="product-card flex flex-col my-4 overflow-hidden">
-      <div className="product-image ">
-        <img
-          src={props.imageUrl}
-          alt="Maningue Cenas"
-          width={220}
-          height={220}
-        />
-      </div>
-      <div className="product-sell flex justify-between py-4 ">
-        <div className="product-name">{props.name}</div>
-        <div className="product-price font-medium">{props.price} MZN</div>
-      </div>
+      <Link href={`/produtos/${props.key}`}>
+        <div className="product-image ">
+          <img
+            src={props.imageUrl}
+            alt="Maningue Cenas"
+            width={220}
+            height={220}
+          />
+        </div>
+        <div className="product-sell flex justify-between items-center">
+          <div className="product-name">{props.name}</div>
+          <div
+            className={
+              hovered
+                ? "buy product-price font-medium"
+                : "product-price font-medium"
+            }
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          >
+            <h4 className={!hovered ? "hidden" : ""}>Eu Quero</h4>
+            <h4>{props.price} MZN</h4>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 }
