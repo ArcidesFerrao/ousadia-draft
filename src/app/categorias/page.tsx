e041e6e9147c4dbaa96fb0733d12f461446acead
ArcidesFerrao <cidesferrao@gmail.com>
2025-01-14 22:31:53 +0200

    category list of products

--- src/app/categorias/page.tsx
+++ src/app/categorias/page.tsx
@@ -1,6 +1,35 @@
-import React from "react";
+// import db from "@/db/db";
+// import { Product } from "@prisma/client";
+// import React, { useEffect, useState } from "react";
+
+// const productsData = async () => {
+//   const data = await db.product.findMany();
+
+//   return data;
+// };
 
 export default function CategoriesPage() {
+  // const [products, setProducts] = useState<Product[]>([]);
+  // const [groupedProducts, setGroupedProducts] = useState<any>({});
+
+  // useEffect(() => {
+  //   const getProducts = async () => {
+  //     const fetchedProducts = await productsData();
+  //     setProducts(fetchedProducts);
+  //     const grouped = fetchedProducts.reduce((acc: any, product: Product) => {
+  //       if (!acc[product.categoryId]) {
+  //         acc[product.category] = [];
+  //       }
+  //       acc[product.category].push(product);
+  //       return acc;
+  //     }, {});
+
+  //     setGroupedProducts(grouped);
+  //   };
+
+  //   getProducts();
+  // }, []);
+
   return (
     <main className="py-4">
       <div className="shirts-list">
