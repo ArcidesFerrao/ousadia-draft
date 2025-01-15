698b7c38f45a132115c33f0688301db5588d8076
ArcidesFerrao <cidesferrao@gmail.com>
2025-01-15 13:09:08 +0200

    fetch products of each category

--- src/app/categorias/page.tsx
+++ src/app/categorias/page.tsx
@@ -1,34 +1,32 @@
-// import db from "@/db/db";
-// import { Product } from "@prisma/client";
-// import React, { useEffect, useState } from "react";
+// "use client";
+import ProductCard from "@/components/ProductCard";
+import db from "@/db/db";
+import { Product } from "@prisma/client";
 
-// const productsData = async () => {
-//   const data = await db.product.findMany();
+async function getProducts() {
+  const products = await db.product.findMany();
 
-//   return data;
-// };
+  return products;
+}
 
-export default function CategoriesPage() {
-  // const [products, setProducts] = useState<Product[]>([]);
-  // const [groupedProducts, setGroupedProducts] = useState<any>({});
+export default async function CategoriesPage() {
+  const products = await getProducts();
+  console.log(products.length);
 
-  // useEffect(() => {
-  //   const getProducts = async () => {
-  //     const fetchedProducts = await productsData();
-  //     setProducts(fetchedProducts);
-  //     const grouped = fetchedProducts.reduce((acc: any, product: Product) => {
-  //       if (!acc[product.categoryId]) {
-  //         acc[product.category] = [];
-  //       }
-  //       acc[product.category].push(product);
-  //       return acc;
-  //     }, {});
-
-  //     setGroupedProducts(grouped);
-  //   };
-
-  //   getProducts();
-  // }, []);
+  const groupedProducts = products.reduce<
+    Record<number | "uncategorized", Product[]>
+  >(
+    (acc, product) => {
+      const categoryId = product.categoryId ?? "uncategorized";
+      if (!acc[categoryId]) {
+        acc[categoryId] = [];
+      }
+      acc[categoryId].push(product);
+      return acc;
+    },
+    { uncategorized: [] }
+  );
+  console.log(groupedProducts);
 
   return (
     <main className="py-4">
@@ -36,25 +34,61 @@ export default function CategoriesPage() {
         <div className="title">
           <h2>Shetas</h2>
         </div>
-        <div className="products-list flex flex-auto justify-between"></div>
+        <div className="products-list flex flex-auto justify-between">
+          {groupedProducts[2]?.map((product) => (
+            <ProductCard
+              key={product.id}
+              name={product.name}
+              price={product.price}
+              imageUrl={product.imageUrl}
+            />
+          ))}
+        </div>
       </div>
       <div className="hats-list">
         <div className="title">
           <h2>Bonés</h2>
         </div>
-        <div className="products-list flex flex-auto justify-between"></div>
+        <div className="products-list flex flex-auto justify-between">
+          {groupedProducts[3]?.map((product) => (
+            <ProductCard
+              key={product.id}
+              name={product.name}
+              price={product.price}
+              imageUrl={product.imageUrl}
+            />
+          ))}
+        </div>
       </div>
       <div className="collabs-list">
         <div className="title">
           <h2>Collabs</h2>
         </div>
-        <div className="products-list flex flex-auto justify-between"></div>
+        <div className="products-list flex flex-auto justify-between">
+          {groupedProducts[4]?.map((product) => (
+            <ProductCard
+              key={product.id}
+              name={product.name}
+              price={product.price}
+              imageUrl={product.imageUrl}
+            />
+          ))}
+        </div>
       </div>
       <div className="brands-list">
         <div className="title">
           <h2>Marcas</h2>
         </div>
-        <div className="products-list flex flex-auto justify-between"></div>
+        <div className="products-list flex flex-auto justify-between">
+          {groupedProducts[5]?.map((product) => (
+            <ProductCard
+              key={product.id}
+              name={product.name}
+              price={product.price}
+              imageUrl={product.imageUrl}
+            />
+          ))}
+        </div>
       </div>
     </main>
   );
