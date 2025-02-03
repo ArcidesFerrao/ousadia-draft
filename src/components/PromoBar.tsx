c432656649b081557ab39b5c53a2870f216641ef
ArcidesFerrao <cidesferrao@gmail.com>
2025-02-03 17:12:20 +0200

    promo bar test

--- src/components/PromoBar.tsx
+++ src/components/PromoBar.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const PromoBar = () =>
   // { message, link }
   {
+    const [isVisible, setIsVisible] = useState(true);
+
+    if (!isVisible) return null;
     return (
       <div>
         {/* <a href={link}>
             {message}
         </a> */}
+        Get 10% off...
+        <button onClick={() => setIsVisible(false)}>X</button>
       </div>
     );
   };
